f98f2e0302c3dc7791f33f17f1a52f37a9bc7280
mateisuica <matei.suica@yahoo.com>
2016-02-23 17:30:46 +0200

    initial

---
+++ Fussball Teams/PlayerDetailsController.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerDetailsController.swift
+//  Fussball Teams
+//
+//  Created by Matei Suica on 23/02/16.
+//  Copyright © 2016 Wirtek. All rights reserved.
+//
+
+import Foundation
---
+++ Fussball Teams/PlayerTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerTableCell.swift
+//  Fussball Teams
+//
+//  Created by Matei Suica on 23/02/16.
+//  Copyright © 2016 Wirtek. All rights reserved.
+//
+
+import Foundation
